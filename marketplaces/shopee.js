3c0a0e07aefb3c505c274072096e0a8ede6bc9f9
d1egopc <diegopcinformatica@gmail.com>
2026-05-26 17:34:44 -0300

    Add files via upload

--- marketplaces/shopee.js
+++ marketplaces/shopee.js
@@ -11,7 +11,8 @@ const {
     ofertaJaExiste,
     classificarCategoriaOferta,
     aplicarFiltrosUniversais,
-    buscarOfertasShopee
+    buscarOfertasShopee,
+    normalizarSessaoId
   } = deps;
 
 
