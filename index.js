5fa916e6fe44ecd1501684ba603de724831b6730
d1egopc <diegopcinformatica@gmail.com>
2026-05-26 16:10:16 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -8902,7 +8902,7 @@ await farejador(clienteId, {
   gerarBuscasGlobais,
   gerarHeadersStealth,
   farejarCuponsMercadoLivre,
-  importarMercadoLivre
+  importarMercadoLivre,
   buscarOfertasShopee
 });
 }
