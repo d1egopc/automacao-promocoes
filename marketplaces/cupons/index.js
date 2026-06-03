1882d222a3eb77618f80092a03bd09ce4c94a627
d1egopc <diegopcinformatica@gmail.com>
2026-06-03 20:38:12 -0300

    Add files via upload

--- marketplaces/cupons/index.js
+++ marketplaces/cupons/index.js
@@ -30,7 +30,7 @@ console.log("🎟️ URL USADA PELO MOTOR:", url);
           "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124 Safari/537.36",
         "Accept-Language": "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7",
         "Accept":
-          "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
+          "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
 
           Cookie: contexto.cookies || ""
       }
