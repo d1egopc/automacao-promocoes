863f31ae1530381028d1f987f469abe7cfd42fd1
d1egopc <diegopcinformatica@gmail.com>
2026-06-11 07:46:13 -0300

    Add files via upload

--- marketplaces/inteligencia/classificador-categorias.js
+++ marketplaces/inteligencia/classificador-categorias.js
@@ -123,7 +123,6 @@ if (contemAlgum(texto, [
 ])) return "Roupas e Calçados Infantil";
 
 
-
 if (contemAlgum(texto, [
   "calcinha", "sutia", "sutiã", "lingerie", "cueca feminina",
   "calcinha boxer", "short sem costura", "she by mash",
@@ -208,7 +207,55 @@ if (contemAlgum(texto, [
   "xadrez feminina",
 
   "moda evangelica",
-  "moda evangélica"
+  "moda evangélica",
+   
+   // ===== REFORÇO =====
+
+  "camisa feminina",
+  "camisete feminina",
+
+  "blazer feminino",
+
+  "kimono feminino",
+
+  "cardigan",
+  "cardigã",
+
+  "top feminino",
+  "top cropped",
+
+  "conjunto moletom feminino",
+
+  "calca flare",
+  "calça flare",
+
+  "calca pantalona",
+  "calça pantalona",
+
+  "jardineira feminina",
+
+  "colete feminino",
+
+  "sobretudo feminino",
+
+  "casaco feminino",
+
+  "parka feminina",
+
+  "anorak feminino",
+
+  "jaqueta jeans feminina",
+
+  "camisa jeans feminina",
+
+  "bermuda feminina",
+
+  "mule feminino",
+
+  "sapatilha feminina",
+
+  "tamanco feminino"
+  
 ])) return "Roupas e Moda Feminina";
 
 if (contemAlgum(texto, [
@@ -225,34 +272,174 @@ if (contemAlgum(texto, [
   "regata masculina", "cueca boxer", "cuecas boxer",
   "boxer masculina", "boxer masculino",
   "moda masculina", "plus size masculino"
+
+     "calca masculina",
+  "calça masculina",
+
+  "calca sarja masculina",
+  "calça sarja masculina",
+
+  "calca moletom masculina",
+  "calça moletom masculina",
+
+  "camisa xadrez masculina",
+  "camisa jeans masculina",
+
+  "blusa masculina",
+  "blusao masculino",
+  "blusão masculino",
+
+  "casaco masculino",
+  "colete masculino",
+
+  "jaqueta jeans masculina",
+  "jaqueta couro masculina",
+
+  "conjunto masculino",
+  "conjunto moletom masculino",
+
+  "terno masculino",
+  "blazer masculino",
+
+  "pijama masculino",
+
+  "sunga",
+  "short praia masculino",
+  "bermuda tactel masculina",
+
+  "carteira masculina",
+  "cinto masculino",
+
+  "meia masculina",
+  "meias masculinas"
+  
 ])) return "Roupas e Moda Masculina";
 
 if (contemAlgum(texto, [
-  "tenis", "tênis", "chinelo", "havaianas", "sandalia", "sandália",
-  "rasteira", "rasteirinha", "tamanco", "sapatilha",
-  "sapatenis", "sapatênis", "crocs", "papete",
-  "mocassim", "loafer", "bota", "botina", "coturno",
-  "sapato", "salto", "salto alto",
-  "mizuno", "asics", "nike", "adidas", "olympikus",
-  "olympicus", "fila", "vizzano", "piccadilly",
-  "puma", "reebok", "new balance", "kappa",
-  "calcado feminino", "calçado feminino",
-  "calcado masculino", "calçado masculino"
+  "tenis", "tênis",
+  "chinelo",
+  "havaianas",
+
+  "sandalia", "sandália",
+  "rasteira", "rasteirinha",
+
+  "tamanco",
+  "sapatilha",
+
+  "sapatenis", "sapatênis",
+
+  "crocs",
+  "papete",
+
+  "mocassim",
+  "loafer",
+
+  "bota",
+  "botina",
+  "coturno",
+
+  "sapato",
+  "salto",
+  "salto alto",
+
+  "mizuno",
+  "asics",
+  "nike",
+  "adidas",
+  "olympikus",
+  "olympicus",
+  "fila",
+  "vizzano",
+  "piccadilly",
+  "puma",
+  "reebok",
+  "new balance",
+  "kappa",
+
+  // reforço
+
+  "pegada",
+  "democrata",
+  "ferracini",
+  "beira rio",
+  "moleca",
+  "molekinha",
+  "molekinho",
+
+  "via marte",
+  "dakota",
+
+  "sandalia plataforma",
+  "sandália plataforma",
+
+  "tenis corrida",
+  "tênis corrida",
+
+  "tenis casual",
+  "tênis casual",
+
+  "tenis esportivo",
+  "tênis esportivo",
+
+  "calcado feminino",
+  "calçado feminino",
+
+  "calcado masculino",
+  "calçado masculino"
 ])) return "Tênis e Chinelos";
 
-  if (contemAlgum(texto, [
-    "smartwatch", "smart watch", "relogio inteligente", "relógio inteligente",
-    "smart band", "amazfit", "galaxy watch"
-  ])) return "Eletrônicos";
+if (contemAlgum(texto, [
+  "smartwatch", "smart watch",
+  "relogio inteligente", "relógio inteligente",
+  "smart band", "smartband",
+  "pulseira inteligente",
+  "monitor cardiaco", "monitor cardíaco",
 
-  if (contemAlgum(texto, [
-    "halter", "haltere", "kettlebell", "musculacao", "musculação",
-    "peso livre", "crossfit", "whey", "creatina", "pre treino",
-    "pré treino", "albumina", "barra de proteina", "barra de proteína",
-    "barra proteica", "faixa elastica", "faixa elástica", "bike",
-    "bicicleta", "spinning", "tapete yoga", "tapete para yoga",
-    "short academia", "camiseta academia", "dry fit", "bola de futebol"
-  ])) return "Esporte e Suplementos";
+  "amazfit", "galaxy watch",
+  "apple watch", "mi band",
+  "haylou", "huawei band",
+
+  "rastreador bluetooth",
+  "airtag", "smart tag", "galaxy smarttag",
+
+  "controle remoto universal",
+  "controle smart",
+  "tomada inteligente",
+  "lampada inteligente", "lâmpada inteligente",
+  "interruptor inteligente",
+  "sensor inteligente",
+  "camera inteligente", "câmera inteligente"
+])) return "Eletrônicos";
+
+if (contemAlgum(texto, [
+  "halter", "haltere", "kettlebell", "musculacao", "musculação",
+  "peso livre", "crossfit", "whey", "creatina", "pre treino",
+  "pré treino", "albumina", "barra de proteina", "barra de proteína",
+  "barra proteica", "faixa elastica", "faixa elástica",
+
+  "bike", "bicicleta", "spinning", "esteira", "eliptico", "elíptico",
+  "ergometrica", "ergométrica",
+
+  "tapete yoga", "tapete para yoga", "yoga", "pilates",
+
+  "short academia", "camiseta academia", "dry fit",
+  "legging esportiva", "top esportivo", "bermuda esportiva",
+
+  "bola de futebol", "bola futebol", "bola futsal",
+  "bola volei", "bola vôlei", "bola basquete",
+
+  "luva academia", "caneleira", "corda de pular",
+  "saco de pancada", "kimono", "tatame",
+
+  "suplemento", "hipercalorico", "hipercalórico",
+  "bcaa", "glutamina", "coqueteleira", "shakeira",
+
+  "tenis corrida", "tênis corrida", "tenis esportivo",
+  "tênis esportivo", "corrida", "caminhada",
+
+  "garrafa termica esportiva", "garrafa esportiva",
+  "squeeze", "mochila hidratacao", "mochila hidratação"
+])) return "Esporte e Suplementos";
 
 
 if (contemAlgum(texto, [
@@ -320,7 +507,30 @@ if (contemAlgum(texto, [
   "medidor de pressão",
 
   "balanca corporal",
-  "balança corporal"
+  "balança corporal",
+
+  "nivea",
+  "loreal", "l'oréal",
+  "elseve",
+  "pantene",
+  "tresemme", "trésemme",
+  "dove",
+
+  "vichy",
+  "la roche",
+  "la roche-posay",
+  "cerave", "ceravee",
+
+  "avon",
+  "natura",
+  "boticario", "boticário",
+  "o boticario", "o boticário",
+
+  "granado",
+  "bio extratus",
+  "wella",
+  "eucerin"
+
 ])) return "Perfumaria, Farmácia e Beleza";
 
 
@@ -347,9 +557,8 @@ if (contemAlgum(texto, [
 ])) return "Ferramentas";
 
   
-// ===== COZINHA / UTILIDADES DOMÉSTICAS =====
-
 if (contemAlgum(texto, [
+  // Cozinha
   "jogo de panelas", "kit panela", "frigideira",
   "panela", "caçarola", "caçarola", "fervedor",
   "faqueiro", "tramontina", "talheres",
@@ -359,27 +568,34 @@ if (contemAlgum(texto, [
   "formas assadeiras", "assadeira", "forma antiaderente",
   "crepeira", "maquina de crepe", "máquina de crepe",
   "garrafa inox", "garrafa termica", "garrafa térmica",
-  "lixeira", "guarda chuva", "cobertor", "manta",
-  "toalhas de banho", "toalha de banho"
-])) return "Casa, Móveis e Decoração";
 
-if (contemAlgum(texto, [
+  // Cama e Banho
+  "cobertor", "manta",
+  "toalhas de banho", "toalha de banho",
+  "colcha", "cobre leito",
+
+  // Decoração
   "tapete", "cortina", "almofada",
-  "sofa", "sofá", "rack",
+  "espelho", "adnet",
+
+  // Móveis
+  "sofa", "sofá",
+  "rack",
   "painel tv", "painel de tv",
   "guarda roupa", "guarda-roupa",
-  "roupeiro", "mesa", "cadeira",
-  "cadeira de escritorio", "cadeira de escritório",
-  "cadeira ergonomica", "cadeira ergonômica",
-  "cadeira executiva", "cadeira presidente",
-  "penteadeira", "comoda", "cômoda",
-  "armário", "armario",
-  "varal", "colcha", "cobre leito",
-  "torneira", "banheiro", "cozinha",
-  "kit churrasco",
-  "utensilios cozinha",
-  "utensílios cozinha",
-  "espelho", "adnet",
+  "roupeiro",
+  "mesa", "cadeira",
+
+  "cadeira de escritorio",
+  "cadeira de escritório",
+  "cadeira ergonomica",
+  "cadeira ergonômica",
+  "cadeira executiva",
+  "cadeira presidente",
+
+  "penteadeira",
+  "comoda", "cômoda",
+  "armario", "armário",
 
   "escrivaninha",
   "nicho",
@@ -392,7 +608,89 @@ if (contemAlgum(texto, [
   "aparador",
   "buffet",
   "criado mudo",
-  "criado-mudo"
+  "criado-mudo",
+
+  // Casa
+  "varal",
+  "lixeira",
+  "guarda chuva",
+  "torneira",
+  "banheiro",
+  "cozinha",
+
+  // Churrasco
+  "kit churrasco",
+
+  // Utilidades
+  "utensilios cozinha",
+  "utensílios cozinha"
+])) return "Casa, Móveis e Decoração";if (contemAlgum(texto, [
+  // Cozinha
+  "jogo de panelas", "kit panela", "frigideira",
+  "panela", "caçarola", "caçarola", "fervedor",
+  "faqueiro", "tramontina", "talheres",
+  "copos", "taças", "tacas", "jogo de copos",
+  "cortador de legumes", "ralador", "fatiador",
+  "marmitas", "potes", "travas hermeticas", "travas herméticas",
+  "formas assadeiras", "assadeira", "forma antiaderente",
+  "crepeira", "maquina de crepe", "máquina de crepe",
+  "garrafa inox", "garrafa termica", "garrafa térmica",
+
+  // Cama e Banho
+  "cobertor", "manta",
+  "toalhas de banho", "toalha de banho",
+  "colcha", "cobre leito",
+
+  // Decoração
+  "tapete", "cortina", "almofada",
+  "espelho", "adnet",
+
+  // Móveis
+  "sofa", "sofá",
+  "rack",
+  "painel tv", "painel de tv",
+  "guarda roupa", "guarda-roupa",
+  "roupeiro",
+  "mesa", "cadeira",
+
+  "cadeira de escritorio",
+  "cadeira de escritório",
+  "cadeira ergonomica",
+  "cadeira ergonômica",
+  "cadeira executiva",
+  "cadeira presidente",
+
+  "penteadeira",
+  "comoda", "cômoda",
+  "armario", "armário",
+
+  "escrivaninha",
+  "nicho",
+  "prateleira",
+  "sapateira",
+  "cabideiro",
+  "cabeceira",
+  "poltrona",
+  "estante",
+  "aparador",
+  "buffet",
+  "criado mudo",
+  "criado-mudo",
+
+  // Casa
+  "varal",
+  "lixeira",
+  "guarda chuva",
+  "torneira",
+  "banheiro",
+  "cozinha",
+
+  // Churrasco
+  "kit churrasco",
+
+  // Utilidades
+  "utensilios cozinha",
+  "utensílios cozinha"
 ])) return "Casa, Móveis e Decoração";
 
 
@@ -618,26 +916,141 @@ if (contemAlgum(texto, [
   "som portatil"
 ])) return "Audio TV";
 
-  if (contemAlgum(texto, [
-    "cafeteira", "air fryer", "fritadeira sem oleo", "fritadeira sem óleo",
-    "microondas", "liquidificador", "batedeira", "sanduicheira",
-    "grill", "panela eletrica", "panela elétrica", "aspirador robo",
-    "robô aspirador", "robo aspirador", "escova secadora"
-  ])) return "Eletroportáteis";
+if (contemAlgum(texto, [
+  "cafeteira",
+  "maquina de cafe", "máquina de café",
 
-  if (contemAlgum(texto, [
-    "geladeira", "freezer", "maquina de lavar", "máquina de lavar",
-    "lava e seca", "fogao", "fogão", "cooktop", "forno eletrico",
-    "forno elétrico", "depurador", "coifa", "climatizador",
-    "ar condicionado", "ventilador", "ventilador de mesa"
-  ])) return "Eletrodomésticos";
+  "air fryer",
+  "fritadeira sem oleo",
+  "fritadeira sem óleo",
+  "fritadeira eletrica", "fritadeira elétrica",
 
+  "microondas",
+
+  "liquidificador",
+  "mixer",
+  "processador alimentos",
+  "processador de alimentos",
+
+  "batedeira",
+
+  "sanduicheira",
+  "grill",
+
+  "panela eletrica",
+  "panela elétrica",
+  "panela pressao eletrica",
+  "panela pressão elétrica",
+  "cooker",
+
+  "aspirador robo",
+  "robô aspirador",
+  "robo aspirador",
+
+  "aspirador de po",
+  "aspirador de pó",
+  "aspirador vertical",
+  "aspirador sem fio",
+
+  "escova secadora",
+  "escova rotativa",
+
+  "chapinha",
+  "prancha cabelo",
+  "prancha de cabelo",
+
+  "secador cabelo",
+  "secador de cabelo",
+
+  "ferro passar",
+  "ferro de passar",
+
+  "vaporizador roupas",
+  "passadeira vapor"
+])) return "Eletroportáteis";
+
+if (contemAlgum(texto, [
+  "geladeira", "refrigerador",
+  "freezer",
+
+  "maquina de lavar", "máquina de lavar",
+  "lavadora de roupas",
+  "lava e seca",
+  "tanquinho",
+
+  "fogao", "fogão",
+  "cooktop",
+
+  "forno eletrico", "forno elétrico",
+  "forno embutir",
+  "forno de embutir",
+
+  "depurador",
+  "coifa",
+
+  "climatizador",
+
+  "ar condicionado",
+  "ar-condicionado",
+  "split inverter",
+  "split",
+
+  "ventilador",
+  "ventilador de mesa",
+  "ventilador coluna",
+  "ventilador de coluna",
+  "ventilador torre",
+  "ventilador de teto",
+
+  "adega climatizada",
+  "cervejeira",
+
+  "lava loucas",
+  "lava-louças",
+  "lava louça",
+
+  "secadora de roupas",
+  "secadora"
+])) return "Eletrodomésticos";
  
   if (contemAlgum(texto, [
-    "mop", "esfregao", "esfregão", "rodo", "limpeza geral",
-    "desinfetante", "multiuso", "amaciante", "downy", "sabao liquido",
-    "sabão líquido", "lava roupas"
-  ])) return "Limpeza";
+  "mop", "esfregao", "esfregão",
+  "rodo", "vassoura",
+  "pá de lixo", "pa de lixo",
+
+  "limpeza geral",
+  "multiuso",
+  "desinfetante",
+  "detergente",
+
+  "amaciante", "downy",
+  "sabao liquido", "sabão líquido",
+  "sabao em po", "sabão em pó",
+  "lava roupas",
+  "lava louças", "lava louca",
+
+  "alvejante",
+  "agua sanitaria", "água sanitária",
+  "cloro",
+
+  "limpa vidro",
+  "limpa piso",
+  "limpa porcelanato",
+  "limpa banheiro",
+
+  "saponaceo", "saponáceo",
+  "removedor",
+  "lustra moveis", "lustra móveis",
+
+  "pano microfibra",
+  "microfibra",
+  "esponja limpeza",
+  "esponja multiuso",
+
+  "limpeza pesada",
+  "kit limpeza",
+  "refil mop"
+])) return "Limpeza";
 
   if (contemAlgum(texto, [
   "molinete", "vara de pesca", "vara telescopica",
