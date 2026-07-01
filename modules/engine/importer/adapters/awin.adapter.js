5f4db5452b3c1336b59782fb753e883cc2f1ca1c
Optimus Promo - D1EGOPC <diegopcinformatica@gmail.com>
2026-07-01 01:24:41 -0300

    Add files via upload

---
+++ modules/engine/importer/adapters/awin.adapter.js
@@ -0,0 +1,11 @@
+async function importarAwinEngine() {
+  return {
+    ok: false,
+    marketplace: "awin",
+    motivo: "adapter_nao_implementado"
+  };
+}
+
+module.exports = {
+  importarAwinEngine
+};
